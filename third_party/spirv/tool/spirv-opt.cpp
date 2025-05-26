955a3cadbd2b4e4ce34443c6b37da6cd72f20a56
BobHuang <git@bobhuang.xyz>
2025-05-27 06:32:29 +0800

    [SPIRV] add bufferization to spirv-opt

--- third_party/spirv/tool/spirv-opt.cpp
+++ third_party/spirv/tool/spirv-opt.cpp
@@ -2,14 +2,17 @@
 
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
+#include "mlir/Dialect/Bufferization/IR/Bufferization.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/Linalg/Passes.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/InitAllPasses.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
 
 int main(int argc, char **argv) {
+  mlir::registerAllPasses();
   mlir::triton::spirv::registerTritonToLinalgPass();
 
   mlir::DialectRegistry registry;
@@ -17,6 +20,7 @@ int main(int argc, char **argv) {
                   mlir::math::MathDialect, mlir::arith::ArithDialect,
                   mlir::scf::SCFDialect, mlir::linalg::LinalgDialect,
                   mlir::func::FuncDialect, mlir::tensor::TensorDialect,
+                  mlir::bufferization::BufferizationDialect,
                   mlir::memref::MemRefDialect>();
 
   return mlir::asMainReturnCode(
