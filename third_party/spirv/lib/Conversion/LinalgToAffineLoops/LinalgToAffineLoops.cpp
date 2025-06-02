6688538e1001334ec75958491fa353011df757fb
BobHuang <git@bobhuang.xyz>
2025-06-03 05:52:19 +0800

    [SPIRV] use linalg::linalgOpToAffineLoops to convert

--- third_party/spirv/lib/Conversion/LinalgToAffineLoops/LinalgToAffineLoops.cpp
+++ third_party/spirv/lib/Conversion/LinalgToAffineLoops/LinalgToAffineLoops.cpp
@@ -1,11 +1,12 @@
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
 #include "mlir/Pass/PassManager.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "mlir/Transforms/Passes.h"
 #include "spirv/include/Conversion/LinalgToAffineLoops/Passes.h"
-
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/Debug.h"
 
-#define DEBUG_TYPE "triton-to-linalg"
+#define DEBUG_TYPE "linalg-to-affine-loops"
 
 namespace mlir::triton::spirv {
 #define GEN_PASS_DEF_LINALGTOAFFINELOOPS
@@ -18,14 +19,24 @@ using namespace mlir::triton::spirv;
 
 namespace {
 
-
-
 struct LinalgToAffineLoops
-    : public mlir::triton::spirv::impl::LinalgToAffineLoopsBase<LinalgToAffineLoops> {
+    : public mlir::triton::spirv::impl::LinalgToAffineLoopsBase<
+          LinalgToAffineLoops> {
 
   void runOnOperation() override {
     auto moduleOp = getOperation();
-    moduleOp.dump();
+    SmallVector<linalg::LinalgOp> genericOps;
+    moduleOp.walk(
+        [&](linalg::GenericOp genericOp) { genericOps.push_back(genericOp); });
+    PatternRewriter rewriter(&getContext());
+    for (auto genericOp : genericOps) {
+      rewriter.setInsertionPoint(genericOp);
+      if (failed(linalg::linalgOpToAffineLoops(rewriter, genericOp))) {
+        llvm::errs() << "Failed to lower to affine loops.\n";
+        return;
+      }
+      rewriter.eraseOp(genericOp);
+    }
   }
 };
 
